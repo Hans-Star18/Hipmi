8defccba865602fe3345369ccff8fc9460bf8cd2
krisnasdna <104411102+krisnasdna@users.noreply.github.com>
2023-05-17 14:40:45 +0000

    update terbaru

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let secItem = document.querySelector("#sec");
 
 
 let countDown = () => {
-  let futureDate = new Date("1 June 2023");
+  let futureDate = new Date("16 June 2023");
   let currentDate = new Date();
   let myDate = futureDate - currentDate;
   //console.log(myDate);
@@ -27,4 +27,35 @@ let countDown = () => {
 countDown()
 
 setInterval(countDown, 1000)
+
+// Promo
+let hariItem = document.querySelector("#hari");
+let jamItem = document.querySelector("#jam");
+let menitItem = document.querySelector("#menit");
+let detikItem = document.querySelector("#detik");
+
+
+let countDownPromo = () => {
+  let futureDate = new Date("23 May 2023");
+  let currentDate = new Date();
+  let myDate = futureDate - currentDate;
+  //console.log(myDate);
+
+  let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+
+  let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+
+  let min = Math.floor(myDate / 1000 / 60) % 60;
+
+  let sec = Math.floor(myDate / 1000) % 60;
+
+  hariItem.innerHTML = days;
+  jamItem.innerHTML = hours;
+  menitItem.innerHTML = min;
+  detikItem.innerHTML = sec;
+}
+
+countDownPromo()
+
+setInterval(countDownPromo, 1000)
 
