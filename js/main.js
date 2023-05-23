3a51f419972648c1c9c129eff9f410df279ff044
krisnasdna <104411102+krisnasdna@users.noreply.github.com>
2023-05-23 13:17:53 +0000

    update

--- js/main.js
+++ js/main.js
@@ -6,27 +6,35 @@ let secItem = document.querySelector("#sec");
 
 let countDown = () => {
   let futureDate = new Date("16 June 2023");
-  let currentDate = new Date();
-  let myDate = futureDate - currentDate;
-  //console.log(myDate);
+  var x = setInterval(() => {
+    let currentDate = new Date();
+    let myDate = futureDate - currentDate;
+  
+    let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+  
+    let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+  
+    let min = Math.floor(myDate / 1000 / 60) % 60;
+  
+    let sec = Math.floor(myDate / 1000) % 60;
+  
+    daysItem.innerHTML = days;
+    hoursItem.innerHTML = hours;
+    minItem.innerHTML = min;
+    secItem.innerHTML = sec;
 
-  let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+    if (myDate < 0){
+      clearInterval(x);
+      daysItem.innerHTML = 0;
+      hoursItem.innerHTML = 0;
+      minItem.innerHTML = 0;
+      secItem.innerHTML = 0;
+    }
 
-  let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
-
-  let min = Math.floor(myDate / 1000 / 60) % 60;
-
-  let sec = Math.floor(myDate / 1000) % 60;
-
-  daysItem.innerHTML = days;
-  hoursItem.innerHTML = hours;
-  minItem.innerHTML = min;
-  secItem.innerHTML = sec;
+  }, 1000);
 }
 
 countDown()
-
-setInterval(countDown, 1000)
 
 // Promo
 let hariItem = document.querySelector("#hari");
@@ -36,26 +44,36 @@ let detikItem = document.querySelector("#detik");
 
 
 let countDownPromo = () => {
-  let futureDate = new Date("23 May 2023");
-  let currentDate = new Date();
-  let myDate = futureDate - currentDate;
-  //console.log(myDate);
+  let futureDate = new Date("28 May 2023");
 
-  let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+  var x = setInterval(() => {
+    let currentDate = new Date();
+    let myDate = futureDate - currentDate;
+  
+    let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+  
+    let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+  
+    let min = Math.floor(myDate / 1000 / 60) % 60;
+  
+    let sec = Math.floor(myDate / 1000) % 60;
+  
+    hariItem.innerHTML = days;
+    jamItem.innerHTML = hours;
+    menitItem.innerHTML = min;
+    detikItem.innerHTML = sec;
 
-  let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+    if (myDate < 0){
+      clearInterval(x);
+      hariItem.innerHTML = 0;
+      jamItem.innerHTML = 0;
+      menitItem.innerHTML = 0;
+      detikItem.innerHTML = 0;
+    }
 
-  let min = Math.floor(myDate / 1000 / 60) % 60;
-
-  let sec = Math.floor(myDate / 1000) % 60;
-
-  hariItem.innerHTML = days;
-  jamItem.innerHTML = hours;
-  menitItem.innerHTML = min;
-  detikItem.innerHTML = sec;
+  }, 1000);
 }
 
-countDownPromo()
+countDownPromo();
 
-setInterval(countDownPromo, 1000)
 
