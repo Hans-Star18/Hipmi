ce5958e8aebcac79e83f0e29f5451bc4d9db0327
krisnasdna <104411102+krisnasdna@users.noreply.github.com>
2023-06-01 15:10:18 +0000

    update landing page

--- js/main.js
+++ js/main.js
@@ -10,13 +10,13 @@ let countDown = () => {
     let currentDate = new Date();
     let myDate = futureDate - currentDate;
   
-    let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+    let days = Math.floor(myDate / (1000 * 60 * 60 * 24));
   
-    let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+    let hours = Math.floor((myDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   
-    let min = Math.floor(myDate / 1000 / 60) % 60;
+    let min = Math.floor((myDate % (1000 * 60 * 60)) / (1000 * 60));
   
-    let sec = Math.floor(myDate / 1000) % 60;
+    let sec = Math.floor((myDate % (1000 * 60)) / 1000);
   
     daysItem.innerHTML = days;
     hoursItem.innerHTML = hours;
